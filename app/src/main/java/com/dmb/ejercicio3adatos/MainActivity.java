69d2bb1d74e6d6a4de188d733f729db2af76c583
jomabe <dmari.scfe11@gmail.com>
2017-11-08 00:58:19 +0100

    Realizando Interfaz. DatePicker Funcional.

---
+++ app/src/main/java/com/dmb/ejercicio3adatos/MainActivity.java
@@ -0,0 +1,64 @@
+package com.dmb.ejercicio3adatos;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton imb;
+    private Calendar myCalendar;
+    private EditText showDate;
+    private DatePickerDialog.OnDateSetListener date;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myCalendar = Calendar.getInstance();
+        imb = (ImageButton)findViewById(R.id.datePicker);
+        showDate = (EditText) findViewById(R.id.date);
+
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+    }
+
+    public void datePicker(View v){
+        imb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.UK);
+
+        showDate.setText(sdf.format(myCalendar.getTime()));
+    }
+}
