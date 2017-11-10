10aaa8e8e83c465317fcbd6bd87b2c23cd7f1e0b
jomabe <dmari.scfe11@gmail.com>
2017-11-10 17:54:32 +0100

    Ejercicio terminado y funcional

--- app/src/main/java/com/dmb/ejercicio3adatos/MainActivity.java
+++ app/src/main/java/com/dmb/ejercicio3adatos/MainActivity.java
@@ -1,11 +1,14 @@
 package com.dmb.ejercicio3adatos;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -14,6 +17,7 @@ import android.widget.RadioButton;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Calendar myCalendar;
     private EditText showDate,et1,et2;
     private RadioButton rb1,rb2;
-    private DatePickerDialog.OnDateSetListener date;
+    private DatePickerDialog date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +41,24 @@ public class MainActivity extends AppCompatActivity {
         rb1 = (RadioButton) findViewById(R.id.rb1);
         rb2 = (RadioButton) findViewById(R.id.rb2);
 
-        date = new DatePickerDialog.OnDateSetListener() {
+        date = new DatePickerDialog(this);
+        DatePicker dp = date.getDatePicker();
+        dp.setMaxDate(myCalendar.getTimeInMillis());
+        date.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
 
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                // TODO Auto-generated method stub
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel();
-            }
 
-        };
+              @Override
+              public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                    int dayOfMonth) {
+                  // TODO Auto-generated method stub
+                  myCalendar.set(Calendar.YEAR, year);
+                  myCalendar.set(Calendar.MONTH, monthOfYear);
+                  myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                  updateLabel();
+              }
+          }
+
+        );
 
     }
 
@@ -57,9 +66,7 @@ public class MainActivity extends AppCompatActivity {
         imb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new DatePickerDialog(MainActivity.this, date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                date.show();
             }
         });
     }
@@ -97,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
         }else if(rb2.isChecked()){
             edit.putString("gender",rb2.getText().toString());
         }else{
-            edit.putString("",null);
+            edit.putString("gender",null);
         }
         edit.apply();
 
